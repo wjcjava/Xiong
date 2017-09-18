f03774f8607943a6417ffbea1805d2113a9226f8
冯玉苗 <364118349@qq.com>
2017-09-18 10:12:14 +0800

    上传啦。。。

--- app/src/main/java/chongci/myapplication/activity/prosentactivity/DengLuActivity.java
+++ app/src/main/java/chongci/myapplication/activity/prosentactivity/DengLuActivity.java
@@ -1,15 +1,73 @@
 package chongci.myapplication.activity.prosentactivity;
 
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
+import com.zhy.android.percent.support.PercentLinearLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import chongci.myapplication.R;
 
 public class DengLuActivity extends AppCompatActivity {
+
+    @BindView(R.id.back)
+    ImageView back;
+    @BindView(R.id.register)
+    TextView register;
+    @BindView(R.id.login_weixin)
+    RadioButton loginWeixin;
+    @BindView(R.id.login_QQ)
+    RadioButton loginQQ;
+    @BindView(R.id.login_weibo)
+    RadioButton loginWeibo;
+    @BindView(R.id.num)
+    EditText num;
+    @BindView(R.id.num_x)
+    ImageView numX;
+    @BindView(R.id.tishi_email)
+    TextView tishiEmail;
+    @BindView(R.id.pass)
+    EditText pass;
+    @BindView(R.id.pass_x)
+    ImageView passX;
+    @BindView(R.id.tishi_pass)
+    TextView tishiPass;
+    @BindView(R.id.forget_pass)
+    TextView forgetPass;
+    @BindView(R.id.dengl)
+    Button dengl;
+    @BindView(R.id.aaaa)
+    PercentLinearLayout aaaa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deng_lu);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.register, R.id.login_weixin, R.id.login_QQ, R.id.login_weibo, R.id.forget_pass})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.register:  //注册
+                break;
+            case R.id.login_weixin:
+                break;
+            case R.id.login_QQ:
+                break;
+            case R.id.login_weibo:
+                break;
+            case R.id.forget_pass:  //忘记密码
+                break;
+        }
     }
 }
--- app/src/main/java/chongci/myapplication/activity/prosentactivity/GeRenActivity.java
+++ app/src/main/java/chongci/myapplication/activity/prosentactivity/GeRenActivity.java
@@ -1,52 +1,65 @@
 package chongci.myapplication.activity.prosentactivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import chongci.myapplication.R;
 
-public class GeRenActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private LinearLayout denglu;
-    private LinearLayout lishi;
-    private LinearLayout shoucang;
-    private LinearLayout shezhi;
+public class GeRenActivity extends AppCompatActivity {
+    @BindView(R.id.back)
+    ImageView back;
+    @BindView(R.id.login_image)
+    ImageView loginImage;
+    @BindView(R.id.login_text)
+    TextView loginText;
+    @BindView(R.id.login)
+    LinearLayout login;
+    @BindView(R.id.imageView)
+    ImageView imageView;
+    @BindView(R.id.textt)
+    TextView textt;
+    @BindView(R.id.hostory)
+    RelativeLayout hostory;
+    @BindView(R.id.imageView1)
+    ImageView imageView1;
+    @BindView(R.id.textt1)
+    TextView textt1;
+    @BindView(R.id.shoucang)
+    RelativeLayout shoucang;
+    @BindView(R.id.imageView2)
+    ImageView imageView2;
+    @BindView(R.id.textt2)
+    TextView textt2;
+    @BindView(R.id.set)
+    RelativeLayout set;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_she_zhi);
-        initView();
+        ButterKnife.bind(this);
     }
 
-    private void initView() {
-        denglu = (LinearLayout) findViewById(R.id.denglu);
-        lishi = (LinearLayout) findViewById(R.id.lishi);
-        shoucang = (LinearLayout) findViewById(R.id.shoucang);
-        shezhi = (LinearLayout) findViewById(R.id.shezhi);
-        denglu.setOnClickListener(this);
-        lishi.setOnClickListener(this);
-        shoucang.setOnClickListener(this);
-        shezhi.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.denglu:
-                startActivity(new Intent(GeRenActivity.this,DengLuActivity.class));
-            break;
-            case R.id.lishi:
-                startActivity(new Intent(GeRenActivity.this,LiShiActivity.class));
-            break;
-            case R.id.shoucang:
-                startActivity(new Intent(GeRenActivity.this,ShouCangActivity.class));
-            break;
-            case R.id.shezhi:
-                startActivity(new Intent(GeRenActivity.this,SheActivity.class));
+    @OnClick({R.id.login_text, R.id.textt, R.id.textt1, R.id.textt2})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.login_text:
+                break;
+            case R.id.textt:
+                break;
+            case R.id.textt1:
+                break;
+            case R.id.textt2:
                 break;
         }
     }
